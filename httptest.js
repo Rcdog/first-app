d1a7728915a2475711e00908dc44dac83a95070c
Ghulam Rachid <rachid.93200@hotmail.fr>
2021-12-28 17:06:38 +0100

    code cour

--- httptest.js
+++ httptest.js
@@ -1,4 +1,7 @@
 const http = require('http');
+const  fs = require('fs');
+const tidt = require('./time.js')
+//  const data = require('./user.json')
 
 const server = http.createServer((req, res) => {
 if (req.url === '/') {
@@ -11,12 +14,42 @@ if (req.url === '/api') {
     res.end();
 }
 
+if (req.url === '/api/time') {
+    res.write('le temps et la date sont:' +tidt.myDateTime());
+    res.end();
+}
+
 if (req.url === '/api/courses') {
     res.write(JSON.stringify([10, 11, 12, 13, 14, 15]));
     res.end();
 }
+
+if (req.url === '/api/blue') {
+    fs.readFile('texte.txt', function(err, data) {
+        if (err) throw err; 
+          res.writeHead(200, {'Content-Type': 'texte.txt'});
+          res.end(data); 
+      });
+}
+
+if (req.url === '/api/json') {
+    fs.readFile('user.json', function(err, data) { 
+    if (err) throw err;
+    res.writeHead(200, {'Content-Type': 'file.json'});
+    res.write('salut  ')
+    res.end(data)  
+    
+});
+}
+
+if (req.url === '/api/jpg') fs.readFile('image.jpg', function(err, data) {
+    if (err) throw err; 
+      res.writeHead(200, {'Content-Type': 'image/jpeg'});
+      res.end(data);
+  });
+  
 });
 
 server.listen(1993);
 
-console.log('ecoute port 1993...')
+console.log('connecter serveur 1993...')
